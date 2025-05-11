ed2864b6ffd970afb523d1a9b814c5339ca088f7
Light2820 <89730592+RedDev28@users.noreply.github.com>
2025-05-11 17:23:40 +0800

    adjust placement

--- JavaScript.js
+++ JavaScript.js
@@ -1,16 +1,14 @@
 function bloomFlower() {
+  if (document.body.classList.contains('not-loaded')) {
 
-          if (document.body.classList.contains('not-loaded')) {
-
-              document.body.classList.remove("not-loaded");
-              document.querySelector('.reloadBtn').textContent = 'Reload Page';
-          } else {
-            location.reload();
-          }
-      }
+      document.body.classList.remove("not-loaded");
+      document.querySelector('.reloadBtn').style.opacity = 0;
+  } 
+}
       
       
 let checker = document.getElementById("checker");
 checker.addEventListener('change', function(e) {
   checker.disabled = true;
+  document.getElementById("label-checker").style.opacity = 0;
 })
